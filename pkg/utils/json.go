0af9f654b52378ca6a7debcafd23c83f4a796db4
Raywall Malheiros <raywall.malheiros@gmail.com>
2025-06-09 18:49:14 -0300

    update

--- pkg/utils/json.go
+++ pkg/utils/json.go
@@ -1,5 +1,15 @@
 package utils
 
-func SaveToJSON(outputFile string, obj interface{}) error {
-	return nil
+import (
+	"encoding/json"
+	"os"
+)
+
+// SaveToJSON is a function to save the performance test result into a JSON file
+func SaveToJSON(filename string, obj interface{}) error {
+	data, err := json.MarshalIndent(obj, "", "  ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filename, data, 0644)
 }
